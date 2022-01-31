8e1349714551221d44c599387381bc707ee29767
coffee100percnt <antiendershow221@gmail.com>
2022-01-31 22:58:36 +0200

    fix ls, remove startf

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import os
 import wget
-import getpass
 import shutil
+import subprocess
 
 version = "0.1-alpha"
-cd = "~"
+cd = os.path.expanduser('~')
 #url_download = "http://cdn.discordapp.com/attachments/900323860904292382/900323946702974986/main.exe"
-print("HIGH SHELL", version, "By 0LungSkill0\n")
+print("HIGH SHELL", version, "By coffee100percnt\n")
+def visualcd(dir):
+    if dir.startswith(os.path.expanduser("~")):
+        idk = cd.split(os.path.expanduser("~"))
+        idk2 = "~" + idk[0]
+        return idk2
 
-while 1 == 1:
-    inp = input("HS "+ cd +" $ ").split()
+while True:
+    inp = input("HS "+ visualcd(cd) +" $ ").split(" ")
     if inp[0] == "help":
         print("help\nversion\ncredits\nexit\nmkdir <file>\nrmdir <file>\nmkfile <file>\nrmfile <file>\ncd <path>\nlist\nrename <file> <new name>\nstartf <file>\ndownload <http url> <file name>\ncopy <file> <new name>")
     elif inp[0] == "version":
@@ -33,21 +38,28 @@ while 1 == 1:
             fl = open(cd + inp[1], "w")
             fl.write("")
             fl.close()
-    elif inp[0] == "rmfile":
+    elif inp[0] == "rm":
         if len(inp) > 1:
             os.remove(cd + inp[1])
     elif inp[0] == "cd":
         if len(inp) > 1:
             cd = inp[1]
-    elif inp[0] == "list":
-        for cont in os.listdir(cd):
-            print(cont)
+    elif inp[0] == "ls":
+        if len(inp) > 1:
+            for cont in os.listdir(cd + "/" + inp[1]):
+                if cont.startswith('.'):
+                    pass
+                else:
+                    print(cont)
+        else:
+            for cont in os.listdir(cd + inp[1]):
+                if cont.startswith('.'):
+                    pass
+                else:
+                    print(cont)
     elif inp[0] == "rename":
         if len(inp) > 2:
                 os.rename(cd + inp[1], cd + inp[2])
-    elif inp[0] == "startf":
-        if len(inp) > 1:
-            os.startfile(cd + inp[1])
     elif inp[0] == "download":
         if len(inp) > 2:
             wget.download(inp[1], cd + inp[2])
@@ -58,4 +70,5 @@ while 1 == 1:
     elif inp[0] == "tty":
         print(os.ctermid())
     else:
-        os.startfile("/bin" + inp[0])
+        cmd = "/bin/" + inp[0]
+        process = subprocess.call(cmd)
