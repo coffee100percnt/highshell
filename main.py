9933123c980588bc5b983245d510f04f01941d9e
coffee100percnt <antiendershow221@gmail.com>
2022-02-01 15:48:09 +0200

    ^D-and-^C-proof

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-version = "0.1-alpha"
+version = "0.2-beta"
 cd = os.path.expanduser('~')
 os.chdir(cd)
 print("HISHELL", version, "By coffee100percnt\n")
@@ -10,25 +10,36 @@ def visualcd(dir):
         idk = cd.split(os.path.expanduser("~"))
         idk2 = "~" + idk[1]
         return idk2
-
-while True:
-    inp = input("HS "+ visualcd(cd) +" $ ").split(" ")
-    if inp[0] == "help":
-        print("help\nversion\ncredits\nexit\nmkdir <file>\nrmdir <file>\nmkfile <file>\nrmfile <file>\ncd <path>\nlist\nrename <file> <new name>\nstartf <file>\ndownload <http url> <file name>\ncopy <file> <new name>")
+def parse(inp, dir):
+    if inp == None:
+        print()
+    elif inp[0] == "help":
+            print("help\nversion\ncredits\nexit\nmkdir <file>\nrmdir <file>\nmkfile <file>\nrmfile <file>\ncd <path>\nlist\nrename <file> <new name>\nstartf <file>\ndownload <http url> <file name>\ncopy <file> <new name>")
     elif inp[0] == "version":
         print("HISHELL", version)
     elif inp[0] == "exit":
         exit()
     elif inp[0] == "credits":
-        out = """Maintainer:
+        print("""Maintainer:
         coffee100percnt (github)
         Contributors:
-        HONAK0 (github)"""
+        HONAK0 (github)""")
     elif inp[0] == "cd":
         if len(inp) > 1:
-            cd += f'/{inp[1]}'
-            os.chdir(cd)
+            dir += f'/{inp[1]}'
+            os.chdir(dir)
         else:
-            cd = os.path.expanduser("~")
+            dir = os.path.expanduser("~")
     else:
         subprocess.run(inp)
+
+while True:
+    try:
+        inp = input("HS "+ visualcd(cd) +" $ ").split(" ")
+        parse(inp, cd)
+    except EOFError:
+        print("\nexit")
+        exit()
+    except KeyboardInterrupt:
+        inp = None
+        parse(inp, cd)
